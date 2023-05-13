4c467e82bbf5eb0429e8fd894e9ab0cc7a9c4282
professorDeveloper <azamovhud007@outlook.com>
2023-05-13 22:57:37 +0500

    added  language of register screen

--- app/src/main/java/com/azamovhudstc/epolisinsurance/ui/screen/register/RegisterScreen.kt
+++ app/src/main/java/com/azamovhudstc/epolisinsurance/ui/screen/register/RegisterScreen.kt
@@ -1,11 +1,17 @@
 package com.azamovhudstc.epolisinsurance.ui.screen.register
 
+import android.content.Context
 import android.os.Bundle
 import android.text.InputType
-import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
+import androidx.fragment.app.Fragment
 import com.azamovhudstc.epolisinsurance.R
+import com.azamovhudstc.epolisinsurance.app.App
 import com.azamovhudstc.epolisinsurance.utils.slideUp
+import kotlinx.android.synthetic.main.fragment_register_screen.*
 import kotlinx.android.synthetic.main.fragment_register_screen.view.*
 
 
@@ -13,10 +19,6 @@ class RegisterScreen : Fragment(R.layout.fragment_register_screen) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.register_phone.setOnClickListener {
-            view.keypad.visibility=View.VISIBLE
-            view.keypad.slideUp(1000,0)
-            view.register_phone.setRawInputType(InputType.TYPE_NULL)
-        }
+        view.send_otp.slideUp(777,0)
     }
 }
