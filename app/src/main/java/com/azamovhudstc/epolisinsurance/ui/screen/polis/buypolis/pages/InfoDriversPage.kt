23bb3c7308eb358a9c6848da2477550d52fb52d6
Stc Android <108933534+professorDeveloper@users.noreply.github.com>
2023-06-03 14:08:50 +0500

    Home screen updated UI

--- app/src/main/java/com/azamovhudstc/epolisinsurance/ui/screen/polis/buypolis/pages/InfoDriversPage.kt
+++ app/src/main/java/com/azamovhudstc/epolisinsurance/ui/screen/polis/buypolis/pages/InfoDriversPage.kt
@@ -1,90 +1,78 @@
 package com.azamovhudstc.epolisinsurance.ui.screen.polis.buypolis.pages
 
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import androidx.annotation.RequiresApi
+import android.widget.Button
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.azamovhudstc.epolisinsurance.R
-import com.azamovhudstc.epolisinsurance.data.model.Driver
-import com.azamovhudstc.epolisinsurance.data.model.TabModel
-import com.azamovhudstc.epolisinsurance.ui.adapter.DriverAdapter
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import com.azamovhudstc.epolisinsurance.ui.adapter.DriverInfoPageItemAdapter
+import com.azamovhudstc.epolisinsurance.utils.invisible
+import com.azamovhudstc.epolisinsurance.utils.visible
+import com.azamovhudstc.epolisinsurance.utils.within
+import com.azamovhudstc.epolisinsurance.viewmodel.InfoDriversPageViewModel
+import com.azamovhudstc.sugurtaapp.utils.showSnack
 import kotlinx.android.synthetic.main.fragment_info_drivers_page.*
-import kotlinx.android.synthetic.main.tab_item_driver.view.*
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class InfoDriversPage : Fragment(R.layout.fragment_info_drivers_page) {
-    private lateinit var categoryList: ArrayList<TabModel>
-    private lateinit var categoryAdapter: DriverAdapter
-    @RequiresApi(Build.VERSION_CODES.M)
+
+    private val SIZE = 5
+    private val driverDetailsAdapter by lazy { DriverInfoPageItemAdapter(
+        SIZE,
+        {
+            viewModel.showSuccess(it)
+        },
+        {
+            viewModel.removeDriver(it)
+        },
+        childFragmentManager,
+        viewLifecycleOwner.lifecycle
+    )}
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        categoryList= ArrayList()
-        categoryList.add(TabModel("1", category = Driver("")))
-        categoryList.add(TabModel("2", category = Driver("")))
-        categoryList.add(TabModel("3", category = Driver("")))
-        categoryList.add(TabModel("4", category = Driver("")))
-        categoryList.add(TabModel("5", category = Driver("")))
-        categoryAdapter = DriverAdapter(requireActivity(),categoryList)
-        driverPager.adapter = categoryAdapter
-        TabLayoutMediator(driverTab, driverPager) { _, _ ->
-
-        }.attach()
-        driverTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val customView = tab?.customView
-                customView?.tabContainer?.setBackgroundResource(R.drawable.bg_dirver_tab_selected)
-                customView?.driver_tab_text?.setTextColor(Color.WHITE)
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                val customView = tab?.customView
-                customView?.driver_tab_text?.setTextColor(Color.BLACK)
-                customView?.tabContainer?.setBackgroundResource(R.drawable.bg_driver_tab_unselected)
-            }
-
-
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-        })
-
-
-
-        setTab()
+        indexLabels = index_container.children.map { it as Button }.toList().toTypedArray()
+        observeViewModel()
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun setTab() {
-        val tabCount = driverTab.tabCount
-        println("tab count $tabCount")
-        println("categoryList ${categoryList.size}")
-        for (i in 0 until tabCount) {
-
-                val tabView =
-                    LayoutInflater.from(requireActivity()).inflate(R.layout.tab_item_driver, null, false)
-                val tab = driverTab.getTabAt(i)
-
-                tab?.customView = tabView
-                tabView.driver_tab_text.text = categoryList[i].name
-                if (i == 0) {
-                    tabView?.tabContainer?.setBackgroundResource(R.drawable.bg_dirver_tab_selected)
-                    tabView?.driver_tab_text?.setTextColor(Color.WHITE)
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            viewModel.driverIndices.collectLatest {
+                indexLabels?.let { array ->
+                    for (i in 0 until it) {
+                        array[i].within {
+                            setOnClickListener { viewModel.showDriver(i) }
+                            text = "${i + 1}"
+                            visible()
+                        }
+                    }
+                    for (i in it until 5) {
+                        array[i].invisible()
+                    }
+                    if (it < (indexLabels?.size ?: 0)) {
+                        array[it].within{
+                            setOnClickListener { viewModel.addDriver() }
+                            text = "+"
+                            visible()
+                        }
+                    }
                 }
-                else {
-                    tabView?.driver_tab_text?.setTextColor(Color.BLACK)
-                    tabView?.tabContainer?.setBackgroundResource(R.drawable.bg_driver_tab_unselected)
-                }
-
+            }
+            viewModel.removedPage.onEach {
+                driverDetailsAdapter.removePage(it)
+            }
+            viewModel.errorMessage.collectLatest {
+                showSnack(message = it)
+            }
+            viewModel.showDriver.collectLatest {
+                driver_viewPager.currentItem = it
+            }
         }
-
     }
-
 
 }
